768a6f508b847191bd4397f0c180b7d36e807d48
Christof Schulze <christof.schulze@gmx.net>
2018-02-04 17:16:36 +0100

    routemgr: re-factor logic for client-recognition / removal. Remove IP on NUD_NOARP

--- src/routemgr.c
+++ src/routemgr.c
@@ -74,41 +74,38 @@ void rtnl_handle_neighbour(routemgr_ctx *ctx, const struct nlmsghdr *nh) {
 	if ( ctx->clientif_index == msg->ndm_ifindex || br_index == msg->ndm_ifindex ) {
 		if (nh->nlmsg_type == RTM_NEWNEIGH && msg->ndm_state & NUD_REACHABLE && tb[NDA_LLADDR]) {
 			if (l3ctx.debug)
-				printf("Status-Change to NUD_REACHABLE, notifying for mac change [%s]\n",  mac_str) ;
+				printf("Status-Change to NUD_REACHABLE, notifying change for client-mac [%s]\n",  mac_str) ;
 			clientmgr_notify_mac(CTX(clientmgr), RTA_DATA(tb[NDA_LLADDR]), msg->ndm_ifindex);
 		}
 
 		if (l3ctx.debug) {
 			if_indextoname(msg->ndm_ifindex, ifname);
-			printf("neighbour [%s] (%s) changed on interface %s, state: %i ... (msgif: %i cif: %i brif: %i)\n", mac_str, ip_str, ifname, msg->ndm_state, msg->ndm_ifindex, ctx->clientif_index, br_index ); // see include/uapi/linux/neighbour.h NUD_REACHABLE for numeric values
+			printf("neighbour [%s] (%s) changed on interface %s, type: %i, state: %i ... (msgif: %i cif: %i brif: %i)\n", mac_str, ip_str, ifname, nh->nlmsg_type, msg->ndm_state, msg->ndm_ifindex, ctx->clientif_index, br_index ); // see include/uapi/linux/neighbour.h NUD_REACHABLE for numeric values
 		}
 
-		switch (nh->nlmsg_type) {
-			case RTM_NEWNEIGH:
-				if (msg->ndm_state & NUD_REACHABLE) {
-					if (tb[NDA_DST] && tb[NDA_LLADDR]) {
-						if (l3ctx.debug)
-							printf("Status-Change to NUD_REACHABLE, ADDING address %s [%s]\n", ip_str, mac_str) ;
-						clientmgr_add_address(CTX(clientmgr), &dst_address, RTA_DATA(tb[NDA_LLADDR]), msg->ndm_ifindex);
-					}
-				}
-				else if (msg->ndm_state & NUD_FAILED) {
-					if (l3ctx.debug)
-						printf("(NEWNEIGH & NUD_FAILED received - sending NS)  %s [%s]\n", ip_str, mac_str);
-					icmp6_send_solicitation(CTX(icmp6), &dst_address);
-				}
-				break;
-			case RTM_DELNEIGH:
-				if (msg->ndm_state & NUD_FAILED) {
-					if (l3ctx.debug)
-						printf("REMOVING (DELNEIGH) %s [%s]\n", ip_str, mac_str);
-					rtmgr_client_remove_address(&dst_address);
-				}
-				break;
-			default:
+		if (msg->ndm_state & NUD_REACHABLE) {
+			if (nh->nlmsg_type == RTM_NEWNEIGH && tb[NDA_DST] && tb[NDA_LLADDR]) {
 				if (l3ctx.debug)
-					printf("Received neither NEWNEIGH not DELNEIGH - doing nothing. nlmsg_type: %i\n", nh->nlmsg_type);
-				break;
+					printf("Status-Change to NUD_REACHABLE, ADDING address %s [%s]\n", ip_str, mac_str) ;
+				clientmgr_add_address(CTX(clientmgr), &dst_address, RTA_DATA(tb[NDA_LLADDR]), msg->ndm_ifindex);
+			}
+		}
+		else if (msg->ndm_state & NUD_FAILED) {
+			if (nh->nlmsg_type == RTM_NEWNEIGH) {
+				if (l3ctx.debug)
+					printf("NEWNEIGH & NUD_FAILED received - sending NS for ip %s [%s]\n", ip_str, mac_str);
+				icmp6_send_solicitation(CTX(icmp6), &dst_address);
+			}
+			else if (nh->nlmsg_type == RTM_DELNEIGH) {
+				if (l3ctx.debug)
+					printf("REMOVING (DELNEIGH) %s [%s]\n", ip_str, mac_str);
+				rtmgr_client_remove_address(&dst_address);
+			}
+		}
+		else if (msg->ndm_state & NUD_NOARP) {
+			if (l3ctx.debug)
+				printf("REMOVING (NOARP) %s [%s]\n", ip_str, mac_str);
+			rtmgr_client_remove_address(&dst_address);
 		}
 	}
 }
