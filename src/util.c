0a2845f67a9a24cf7da3462b742be9ad507e2832
Christof Schulze <christof.schulze@gmx.net>
2018-04-19 22:14:03 +0200

    move static char* out of print_ip

--- src/util.c
+++ src/util.c
@@ -8,9 +8,10 @@
 
 /* print a human-readable representation of an in6_addr struct to stdout
 ** */
+static char ipaddress_buffer[INET6_ADDRSTRLEN+1];
+
 const char *print_ip(const struct in6_addr *addr) {
-	static char a1[INET6_ADDRSTRLEN+1];
-	return inet_ntop(AF_INET6, &(addr->s6_addr), a1, INET6_ADDRSTRLEN);
+	return inet_ntop(AF_INET6, &(addr->s6_addr), ipaddress_buffer, INET6_ADDRSTRLEN);
 }
 
 void log_debug(const char *format, ...) {
