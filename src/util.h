0ac63b6dca0994f6d9aac2b3d223857feb543690
Christof Schulze <christof.schulze@gmx.net>
2018-04-29 22:40:17 +0200

    util.h: reformat source

--- src/util.h
+++ src/util.h
@@ -5,7 +5,7 @@
 struct in_addr extractv4_v6(const struct in6_addr *src);
 void mapv4_v6(const struct in_addr *src, struct in6_addr *dst);
 const char *print_ip4(const struct in_addr *addr);
-const char* print_ip(const struct in6_addr *addr);
+const char* print_ip(const struct in6_addr* addr);
 void log_verbose(const char *format, ...);
 void log_debug(const char *format, ...);
 
