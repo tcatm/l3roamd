ca3278bd8ecd0d88b3434143b988700907fcd4c1
Stefan Haun <tux@netz39.de>
2019-06-01 00:25:17 +0200

    Add SPDX license identifier

--- src/genl.h
+++ src/genl.h
@@ -1,3 +1,10 @@
+/*
+ * This file is part of project l3roamd. It's copyrighted by the contributors
+ * recorded in the version control history of the file, available from
+ * its original location https://github.com/freifunk-gluon/l3roamd.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 #pragma once
 
 #include <netlink/genl/genl.h>
